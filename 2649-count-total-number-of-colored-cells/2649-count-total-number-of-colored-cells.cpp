f16ac15bc1ccc32330bc384fe06f556e2aa15dea
Rishit Aggarwal <rishitaggarwal1@gmail.com>
2025-03-05 13:27:16 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.36%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long ans=1;
+        if(n==1)
+            return 1;
+        for(int i=2;i<=n;i++)
+        {
+            ans+=(i-1)*4;
+        }
+        return ans;
+    }
+};
