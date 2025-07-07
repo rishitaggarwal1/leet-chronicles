02a50e80c1e2f79a39f92fb1e2b7307c3018ad2a
Rishit Aggarwal <rishitaggarwal1@gmail.com>
2025-07-07 20:53:42 +0530

    Time: 2657 ms (5.04%), Space: 450.7 MB (5.62%) - LeetHub

---
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    static bool comp(vector<int> a, vector<int> b)
+    {
+        return (a[0]<b[0]) || (a[0]==b[0] && a[1]<b[1]);
+    }
+    int maxEvents(vector<vector<int>>& events) {
+        int n=events.size();
+        sort(events.begin(),events.end(),comp);
+        for(int i=0;i<n;i++)
+        {
+            cout<<events[i][0] <<"  "<<events[i][1]<<endl;
+        }
+        int md=0;
+        for(int i=0;i<n;i++)
+        {
+            if(md<events[i][1])
+                md=events[i][1];
+        }
+        int ans=0;
+        priority_queue<int, vector<int>, greater<> > pq;
+        for(int i=0,j=0;i<=md;i++)
+        {
+            while(j<n && events[j][0]<=i)
+            {
+                pq.emplace(events[j][1]);
+                j++;
+            }
+            while(!pq.empty() && pq.top()<i)
+                pq.pop();
+            if(!pq.empty())
+            {
+                pq.pop();
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
